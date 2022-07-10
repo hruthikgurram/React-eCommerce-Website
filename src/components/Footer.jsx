3fcbb25d00a3ad53b7c1d57e051b1cec099ed4c2
hruthikgurram <79998454+hruthikgurram@users.noreply.github.com>
2022-07-10 19:42:05 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,124 @@
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons";
+import styled from "styled-components"
+import { mobile } from '../responsive';
+
+const Container = styled.div`
+    display: flex;
+    ${mobile({flexDirection: "column"})}
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+    display: flex;
+`;
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+    ${mobile({display: "none"})}
+`;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+    ${mobile({backgroundColor: "#fff8f8"})}
+`;
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 60%;
+    height: 30%;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+        <Left>
+            <Logo>HRUTHIK.</Logo>
+            <Desc>There are many variations of Lorem Ipsum possible, but the majority have suffered alteration in some
+                form, by injected humour, or randamized words which don't even look slightly believable.
+            </Desc>
+            <SocialContainer>
+                <SocialIcon color="3B5999">
+                    <Facebook/>
+                </SocialIcon>
+                <SocialIcon color="E4405F">
+                    <Instagram/>
+                </SocialIcon>
+                <SocialIcon color="55ACEE">
+                    <Twitter/>
+                </SocialIcon>
+                <SocialIcon color="E60023">
+                    <Pinterest/>
+                </SocialIcon>
+            </SocialContainer>
+        </Left>
+        <Center>
+            <Title>Useful Links</Title>
+            <List>
+                <ListItem>Home</ListItem>
+                <ListItem>Cart</ListItem>
+                <ListItem>Men Fashion</ListItem>
+                <ListItem>Women Fashion</ListItem>
+                <ListItem>Accesories</ListItem>
+                <ListItem>My account</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Wishlist</ListItem>
+                <ListItem>Terms</ListItem>
+            </List>
+        </Center>
+        <Right>
+            <Title>Contact</Title>
+            <ContactItem><Room style={{marginRight: "10px"}}/>Near Puri Gate,IIT Kharagpur 721302</ContactItem>
+            <ContactItem><Phone style={{marginRight: "10px"}}/>+91 9885565776</ContactItem>
+            <ContactItem><MailOutline style={{marginRight: "10px"}}/>queries@hruthik.dev</ContactItem>
+            <Payment src="https://i.ibb.co/tPgDgbD/48-480088-payment-method-credit-card-master-card-hd-png.png"/>
+        </Right>
+    </Container>
+  );
+};
+
+export default Footer
